fd07b193b56626254e344a82a3665e33d859f443
André Pinheiro <andreftmarcelo@gmail.com>
2020-07-05 16:25:11 +0100

    add style to footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,9 +3,8 @@ import './style.css'
 
 const Footer = () => (
     <footer>
-        Follow us on Twitter! <br></br>
         <i>Copyright &copy; 2003-2020 The SuperTux Team</i> <br></br>
-        This page is licensed under CC BY-SA 3.0
+        Page licensed under <a href="http://google.com">CC BY-SA 3.0</a>
     </footer>
 )
 
