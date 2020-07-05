fcb5937c441665503cf242591ed7d8cdbbc71d99
André Pinheiro <andreftmarcelo@gmail.com>
2020-07-05 15:57:16 +0100

    Add footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import './style.css'
 
 const Footer = () => (
-    <footer></footer>
+    <footer>
+        Follow us on Twitter! <br></br>
+        <i>Copyright &copy; 2003-2020 The SuperTux Team</i> <br></br>
+        This page is licensed under CC BY-SA 3.0
+    </footer>
 )
 
 export default Footer
