5dac682b4662071e6837713f222a4f90bdbab52a
André Pinheiro <andreftmarcelo@gmail.com>
2020-07-21 17:22:05 +0100

    Add coin an block

--- src/pages/home/welcome/index.js
+++ src/pages/home/welcome/index.js
@@ -5,6 +5,8 @@ import tux from '../../../img/welcome/Bigtuxrun1.png'
 import jumpy from '../../../img/welcome/down-1.png'
 import snowball from '../../../img/welcome/snowball-0.png'
 import ice from '../../../img/welcome/ice.png'
+import coin from '../../../img/welcome/coin.png'
+import block from '../../../img/welcome/block.png'
 
 const Welcome = () => (
     <div className="slide-welcome">
@@ -17,6 +19,9 @@ const Welcome = () => (
         <img className="tux-running" src={tux}></img>
         <img className="jumpy" src={jumpy}></img>
         <img className="snowball" src={snowball}></img>
+        <img className="coin1" src={coin}></img>
+        <img className="coin2" src={coin}></img>
+        <img className="block" src={block}></img>
     </div>
 )
 
