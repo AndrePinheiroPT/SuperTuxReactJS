aa02781c6951851a74f6d3456c5a4aeebe83cbe0
André Pinheiro <andreftmarcelo@gmail.com>
2020-08-20 19:01:14 +0100

    Add animations on welcome and download slide

--- src/pages/home/welcome/index.js
+++ src/pages/home/welcome/index.js
@@ -10,10 +10,18 @@ import block from '../../../img/welcome/block.png'
 
 const Welcome = () => (
     <div className="slide-welcome">
-        <h1>
-            Welcome to the <br></br>
-            Supertux Website
-        </h1>
+        <div className="div-welcome">
+            <h1>
+                Welcome to the <br></br>
+                Supertux Website
+            </h1>
+            <p>
+                SuperTux is a open-source classic 2D jump'n run <br></br>
+                sidescroller game in a style similar to the original Super Mario games. <br></br>
+                <a href="http://google.com">Learn more…</a>
+            </p>
+        </div>
+        
         <img className="ice" src={ice}></img>
         <div className="ground-snow"></div>
         <img className="tux-running" src={tux}></img>
