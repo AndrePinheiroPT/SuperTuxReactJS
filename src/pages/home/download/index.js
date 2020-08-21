a62399893c37951b28daa64986a528ec397f18a7
André Pinheiro <andreftmarcelo@gmail.com>
2020-08-21 12:35:45 +0100

    Add new enemies and supertux font

--- src/pages/home/download/index.js
+++ src/pages/home/download/index.js
@@ -5,6 +5,9 @@ import windows from '../../../img/windows.png'
 import macos from '../../../img/macos.png'
 import linux from '../../../img/linux.png'
 import haywire from '../../../img/download/haywire1.png'
+import buttjump1 from '../../../img/download/buttjump1.png'
+import spiky1 from '../../../img/download/spiky1.png'
+import iceblock1 from '../../../img/download/iceblock1.png'
 
 const Welcome = () => (
     <div className="slide-download">
@@ -20,16 +23,37 @@ const Welcome = () => (
             <br></br>
             <div className="windows-div">
                 <img class="windows-img" src={windows}></img>
-                <h2 className="single-windows">Windows</h2>
+                <h2 className="single-windows">
+                    Windows
+                    <span className="span-text">
+                        SuperTux v0.6.2 (.msi)<br></br> 
+                        64-bit
+                    </span>
+                </h2>
             </div><div className="linux-div">
                 <img class="linux-img" src={linux}></img>
-                <h2 className="single-linux">Linux</h2>
+                <h2 className="single-linux">
+                    Linux
+                    <span className="span-text">
+                        SuperTux v0.6.2 <br></br>
+                        (.Applmage)
+                    </span>
+                </h2>
             </div><div className="macos-div">
                 <img class="macos-img" src={macos}></img>
-                <h2 className="single-macos ">MacOS</h2>
+                <h2 className="single-macos ">
+                    MacOS
+                    <span className="span-text">
+                        SuperTux v0.6.2 <br></br>
+                        (.dmg)
+                    </span>
+                </h2>
             </div>
         </div>
 
+        <img class="buttjump1" src={buttjump1}></img>
+        <img class="iceblock1" src={iceblock1}></img>
+        <img class="spiky1" src={spiky1}></img>
         <img class="haywire1" src={haywire}></img>
         <img class="haywire2" src={haywire}></img>
     </div>
